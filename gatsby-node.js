c047e7111f342ff11e19d0f852b5f16b382c784b
grgvn <gregoiregavin@gmail.com>
2022-07-31 20:08:25 +0200

    gatsby-node isotope-layout

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,41 +1,14 @@
-exports.createSchemaCustomization = ({ actions }) => {
-    
-    const { createTypes } = actions
-
-    createTypes(`
-      type SiteSiteMetadata {
-        author: Author
-        siteUrl: String
-        social: Social
-      }
-  
-      type Author {
-        name: String
-        summary: String
-      }
-  
-      type Social {
-        twitter: String
-      }
-  
-      type MarkdownRemark implements Node {
-        frontmatter: Frontmatter
-        fields: Fields
-      }
-  
-      type Frontmatter {
-        title: String
-        description: String
-        date: Date @dateformat
-      }
-  
-      type Fields {
-        slug: String
-      }
-
-      type StrapiAaaactivite implements Node @infer {
-        unAutreTitre: String!
-      }
-
-    `)
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /isotope-layout/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
   }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,105 +5,16 @@ import Seo from "../components/seo"
 import Isotope from 'isotope-layout/js/isotope';
 import { useEffect } from 'react';
 
+const isBrowser = () => typeof window !== "undefined"
+
 const Explorer = ({ data, location }) => {
 
   const siteTitle = data.site.siteMetadata?.title || `Title`
-  const allActivites = data.allStrapiActivite?.edges
-  const allConcepts = data.allStrapiConcept?.edges
-  const allSequences = data.allStrapiSequence?.edges
-
-  const GridConstruction = () => {
-
-    useEffect(() => {
-
-      console.log('yo')
-
-      // Isotope initialisation
-      let iso = new Isotope('.grid', {
-        itemSelector: '.element-item',
-        layoutMode: 'fitRows'
-      })
-
-      // Filtering functions
-      let filters = {
-        showActivities: function (itemElem) {
-          let type = itemElem.querySelector('.type').textContent
-          return type.match(/Activité/)
-        },
-        showConcepts: function (itemElem) {
-          let type = itemElem.querySelector('.type').textContent
-          return type.match(/Concept/)
-        },
-        showSequences: function (itemElem) {
-          let type = itemElem.querySelector('.type').textContent
-          return type.match(/Séquence/)
-        }
-      }
-
-      // Button click binding
-      let filtersBlock = document.querySelector('.filters');
-
-      filtersBlock.addEventListener('click', (e) => {
-
-        // Bind to filtering function
-        let filterValue = e.target.getAttribute('data-filter')
-        filterValue = filters[filterValue] || filterValue
-
-        // Filtering
-        iso.arrange({ filter: filterValue })
-      })
-
-    })
-
-    return (
-      <div className='grid' style={{ border: '1px solid #333', minHeight: '531.251px' }}>
-        {
-          allActivites ? 
-            allActivites.map(activite => (
-              <a href={activite.node.slug} key={activite.node.id}>
-                <span style={{ display: 'block', padding: '15px'}} className='element-item' key={activite.node.id}>
-                  <h3 className='name'>{activite.node.titre}</h3>
-                  <p className='type'>Activité</p>
-                </span>
-              </a>
-            )) : ''
-        }
-        {
-          allConcepts ? 
-            allConcepts.map(concept => (
-              <a href={concept.node.slug} key={concept.node.id}>
-                <span style={{ display: 'block', padding: '15px' }} className='element-item' key={concept.node.id}>
-                  <h3 className='name'>{concept.node.titre}</h3>
-                  <p className='type'>Concept</p>
-                </span>
-              </a>
-            )) : ''
-        }
-        {
-          allSequences ?
-            allSequences.map(sequence => (
-              <a href={sequence.node.slug} key={sequence.node.id}>
-                <span style={{ display: 'block', padding: '15px' }} className='element-item' key={sequence.node.id}>
-                  <h3 className='name'>{sequence.node.titre}</h3>
-                  <p className='type'>Séquence</p>
-                </span>
-              </a>
-            )) : ''
-        }
-      </div>
-    )
-  }
 
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title='Explorer' />
-      <div className='button-group filters'>
-        <button className='button' data-filter='*'>Tout montrer</button>
-        <button className='button' data-filter='showActivities'>Activités</button>
-        <button className='button' data-filter='showConcepts'>Concepts</button>
-        <button className='button' data-filter='showSequences'>Séquences</button>
-      </div>
-      <GridConstruction />
+      <h1>yo</h1>
     </Layout>
   )
 }
